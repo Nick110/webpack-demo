95869e9056923990115e737d01829f6f69075444
Nick110 <1589744751@qq.com>
2020-02-26 20:40:43 +0800

    add iconfont

--- src/components/BottomMenu/index.tsx
+++ src/components/BottomMenu/index.tsx
@@ -1,9 +1,10 @@
-import React, { SFC } from 'react';
+import React, {useState, SFC } from 'react';
 import Styles from './index.less';
 
 interface Item {
     name: string;
     url: string;
+    icon: string;
     key: number
 }
 
@@ -13,12 +14,28 @@ interface IProps {
 }
 
 const bottomMenu: SFC<IProps>  = function (props) {
-    let {items = [{key: 1, name: '发现', url: '/discover'}, {key: 2, name: '我的', url: '/my'}, {key: 3, name: '云村', url: '/cloud'}, {key: 4, name: '账号', url: '/account'}]} = props;
+    let {active, items = [
+        {key: 1, name: '发现', icon: 'discover', url: '/discover'}, 
+        {key: 2, name: '视频', icon: 'video', url: '/video'}, 
+        {key: 3, name: '我的', icon: 'my', url: '/my'}, 
+        {key: 4, name: '云村', icon: 'yuncun', url: '/cloud'}, 
+        {key: 5, name: '账号', icon: 'account', url: '/account'}
+    ]} = props;
+
+    const [nowActive, setActive] = useState(active);
+
+    const activeChange = (key: number) => {
+        setActive(key);
+    }
     
     return (
         <div className={Styles.bottomMenu}>
             {
-                items.map((item) => <div key={item.key}>{item.name}</div>)
+                items.map((item) => <div key={item.key} className={nowActive === item.key ? Styles.active : ''} onClick={() => activeChange(item.key)}>
+                    <i className={`iconfont icon-${item.icon}`}></i>
+                    <br/>
+                    <span>{item.name}</span>
+                </div>)
             }
 
         </div>
